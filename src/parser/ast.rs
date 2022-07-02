9086d13be3dc988ff0139a534fecb5bf537ecdd8
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2022-07-02 14:30:58 +0000

    Add barebones early AST

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,11 +1,16 @@
 use crate::common::location::Located;
 
 /// The type for all identifiers
-pub type Identifier = String;
+pub type EarlyIdentifier = String;
 
-/// A static 2-adic operator
+/// The type for function parameters
+pub type EarlyParams = Vec<Located<EarlyExpression>>;
+
+pub type EarlyProgram = Vec<Located<EarlyNode>>;
+
+/// A 2-adic operator
 #[derive(Debug)]
-pub enum StaticBinOp {
+pub enum EarlyBinOp {
     Plus,
     Minus,
     Div,
@@ -17,32 +22,113 @@ pub enum StaticBinOp {
     SmallerEq,
     Greater,
     GreaterEq,
+    Equals,
+    NEquals,
+    Concat,
+    BitOr,
+    BitAnd,
+    BitXOr,
 }
 
-/// A static 1-adic operator
+/// A 1-adic operator
 #[derive(Debug)]
-pub enum StaticMonOp {
+pub enum EarlyMonOp {
     Minus,
     Not,
 }
 
-// A static immediate
+/// A immediate
 #[derive(Debug)]
-pub enum StaticImmediate {
+pub enum EarlyImmediate {
     Int(usize),
+    Bool(bool),
 }
 
 #[derive(Debug)]
-pub enum StaticExpression {
-    Ident(Located<Identifier>),
-    Immediate(Located<StaticImmediate>),
+pub enum EarlyIndex {
+    Range {
+        lhs: Option<Located<EarlyExpression>>,
+        rhs: Option<Located<EarlyExpression>>,
+        rhs_iinclusive: bool,
+    },
+    Simple(Located<EarlyExpression>),
+}
+
+#[derive(Debug)]
+pub enum EarlyExpression {
+    Ident(Located<EarlyIdentifier>),
+    Immediate(Located<EarlyImmediate>),
     MonOp {
-        operand: Box<Located<StaticExpression>>,
-        operator: Located<StaticMonOp>,
+        operand: Box<Located<EarlyExpression>>,
+        operator: Located<EarlyMonOp>,
     },
     BinOp {
-        lhs: Box<Located<StaticExpression>>,
-        rhs: Box<Located<StaticExpression>>,
-        operator: Located<StaticBinOp>,
+        lhs: Box<Located<EarlyExpression>>,
+        rhs: Box<Located<EarlyExpression>>,
+        operator: Located<EarlyBinOp>,
     },
+    FuncCall {
+        func_name: Located<EarlyIdentifier>,
+        static_params: EarlyParams,
+        runtime_params: EarlyParams,
+    },
+    Index {
+        lhs: Box<Located<EarlyExpression>>,
+        index: Box<Located<EarlyIndex>>,
+    },
+}
+
+#[derive(Debug)]
+pub enum EarlyStamementLhs {
+    Ident(Located<EarlyIdentifier>),
+    // TODO Add slice as a LHS?
+}
+
+#[derive(Debug)]
+pub enum EarlyStatement {
+    Affect {
+        lhs: EarlyStamementLhs,
+        rhs: Box<Located<EarlyExpression>>,
+    },
+    IfThenElse {
+        condition: Box<Located<EarlyExpression>>,
+        if_block: Located<Block>,
+        else_block: Option<Located<Block>>,
+    }
+}
+
+pub type Block = Vec<Located<EarlyStatement>>;
+
+#[derive(Debug)]
+pub struct EarlyRuntimeArg {
+    name: Located<EarlyIdentifier>,
+    size: Option<usize>,
+}
+
+#[derive(Debug)]
+pub enum EarlyStaticType {
+    Int,
+    Bool,
+}
+
+#[derive(Debug)]
+pub struct EarlyStaticArg {
+    name: Located<EarlyIdentifier>,
+    typ: Option<Located<EarlyStaticType>>,
+}
+
+#[derive(Debug)]
+pub struct EarlyFunc {
+    name: Located<EarlyIdentifier>,
+    args: Located<Vec<Located<EarlyStaticArg>>>,
+    return_type: Located<EarlyStaticType>,
+}
+
+#[derive(Debug)]
+pub struct EarlyNode {
+    name: Located<EarlyIdentifier>,
+    static_args: Located<Vec<Located<EarlyStaticArg>>>,
+    runtime_args: Located<Vec<Located<EarlyRuntimeArg>>>,
+    runtime_outs: Located<Vec<Located<EarlyRuntimeArg>>>,
+    block: Located<Block>,
 }
