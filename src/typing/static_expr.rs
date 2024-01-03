af65008b1b710d1bef71f20ddafa70ea546a11b1
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2024-01-03 14:36:18 +0000

    Static typing: ife and monop

--- src/typing/static_expr.rs
+++ src/typing/static_expr.rs
@@ -1,18 +1,28 @@
-use crate::Located;
 use crate::parser::ast::*;
 use crate::typing::*;
+use crate::Located;
 use std::collections::HashMap;
 
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Ord, Eq)]
 pub enum StaticTypingError {
     IdentNotFound,
+    Monop,
+    ConditionNotBool,
+    ConditionTypeMismatch,
 }
 
 pub type StaticIdentifier = String;
 
-pub type StaticLocated<T> = Located<StaticType, T>;
+pub type StaticLocated<T> = Located<(), T>;
+pub type StaticTypedLocated<T> = Located<StaticType, T>;
 
 impl<T> StaticLocated<T> {
+    pub fn from_early(early: EarlyLocated<EarlyStaticExpression>, exp: T) -> Self {
+        Self::__from_loc(exp, (), early.loc)
+    }
+}
+
+impl<T> StaticTypedLocated<T> {
     pub fn from_early(early: EarlyLocated<EarlyStaticExpression>, exp: T, typ: StaticType) -> Self {
         Self::__from_loc(exp, typ, early.loc)
     }
@@ -21,7 +31,7 @@ impl<T> StaticLocated<T> {
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Ord, Eq)]
 pub enum StaticMonop {
     Not,
-    Neg
+    Neg,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Ord, Eq)]
@@ -43,18 +53,18 @@ pub enum StaticExpression {
     IntLiteral(u64),
     BoolLiteral(bool),
     MonOp {
-        operand: Box<StaticLocated<StaticExpression>>,
+        operand: Box<StaticTypedLocated<StaticExpression>>,
         operator: StaticLocated<StaticMonop>,
     },
     BinOp {
-        lhs: Box<StaticLocated<StaticExpression>>,
-        rhs: Box<StaticLocated<StaticExpression>>,
+        lhs: Box<StaticTypedLocated<StaticExpression>>,
+        rhs: Box<StaticTypedLocated<StaticExpression>>,
         operator: StaticLocated<StaticBinop>,
     },
     IfThenElse {
-        condition: Box<StaticLocated<StaticExpression>>,
-        if_block: Box<StaticLocated<StaticExpression>>,
-        else_block: Box<StaticLocated<StaticExpression>>,
+        condition: Box<StaticTypedLocated<StaticExpression>>,
+        if_block: Box<StaticTypedLocated<StaticExpression>>,
+        else_block: Box<StaticTypedLocated<StaticExpression>>,
     },
 }
 
@@ -62,23 +72,97 @@ pub enum StaticExpression {
 pub fn type_static(
     exp: EarlyLocated<EarlyStaticExpression>,
     env: HashMap<String, StaticType>,
-) -> Result<StaticExpression, StaticTypingError> {
+) -> Result<StaticTypedLocated<StaticExpression>, StaticLocated<StaticTypingError>> {
+    let loc = exp.loc;
     match exp.inner {
         EarlyStaticExpression::Ident(id) => env
             .get(&id)
-            .ok_or(StaticTypingError::IdentNotFound)
-            .map(|v| ),
-        EarlyStaticExpression::Literal(lit) => 
-            match lit {
-                EarlyStaticLiteral::Int(_) => Ok(StaticType::Int),
-                EarlyStaticLiteral::Bool(_) => Ok(StaticType::Bool),
-            },
-        EarlyStaticExpression::MonOp { operand, operator } => todo!(),
+            .ok_or(StaticLocated::__from_loc(
+                StaticTypingError::IdentNotFound,
+                (),
+                loc.clone(),
+            ))
+            .map(|v| StaticTypedLocated::__from_loc(StaticExpression::Ident(id), *v, loc)),
+        EarlyStaticExpression::Literal(lit) => match lit {
+            EarlyStaticLiteral::Int(i) => Ok(StaticTypedLocated::__from_loc(
+                StaticExpression::IntLiteral(i),
+                StaticType::Int,
+                loc,
+            )),
+            EarlyStaticLiteral::Bool(b) => Ok(StaticTypedLocated::__from_loc(
+                StaticExpression::BoolLiteral(b),
+                StaticType::Bool,
+                loc,
+            )),
+        },
+        EarlyStaticExpression::MonOp { operand, operator } => {
+            let operand_exp = type_static(*operand, env)?;
+            match (operand_exp.custom, operator.inner) {
+                (StaticType::Int, EarlyStaticMonOp::Minus) => Ok(StaticTypedLocated::__from_loc(
+                    StaticExpression::MonOp {
+                        operand: Box::new(operand_exp),
+                        operator: StaticLocated::__from_loc(
+                            StaticMonop::Neg,
+                            (),
+                            operator.loc.clone(),
+                        ),
+                    },
+                    StaticType::Int,
+                    loc.clone(),
+                )),
+                (StaticType::Bool, EarlyStaticMonOp::Not) => Ok(StaticTypedLocated::__from_loc(
+                    StaticExpression::MonOp {
+                        operand: Box::new(operand_exp),
+                        operator: StaticLocated::__from_loc(
+                            StaticMonop::Not,
+                            (),
+                            operator.loc.clone(),
+                        ),
+                    },
+                    StaticType::Bool,
+                    loc.clone(),
+                )),
+                _ => Err(StaticLocated::__from_loc(
+                    StaticTypingError::Monop,
+                    (),
+                    loc.clone(),
+                )),
+            }
+        }
         EarlyStaticExpression::BinOp { lhs, rhs, operator } => todo!(),
         EarlyStaticExpression::IfThenElse {
             condition,
             if_block,
             else_block,
-        } => todo!(),
+        } => {
+            let condition_exp = type_static(*condition, env.clone())?;
+            if condition_exp.custom != StaticType::Bool {
+                return Err(StaticLocated::__from_loc(
+                    StaticTypingError::ConditionNotBool,
+                    (),
+                    condition_exp.loc.clone(),
+                ));
+            }
+            let if_exp = type_static(*if_block, env.clone())?;
+            let else_exp = type_static(*else_block, env.clone())?;
+            if if_exp.custom == else_exp.custom {
+                let return_type = if_exp.custom.clone();
+                Ok(StaticTypedLocated::__from_loc(
+                    StaticExpression::IfThenElse {
+                        condition: Box::new(condition_exp),
+                        if_block: Box::new(if_exp),
+                        else_block: Box::new(else_exp),
+                    },
+                    return_type,
+                    loc.clone(),
+                ))
+            } else {
+                Err(StaticLocated::__from_loc(
+                    StaticTypingError::ConditionTypeMismatch,
+                    (),
+                    loc.clone(),
+                ))
+            }
+        }
     }
 }
